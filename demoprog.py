1e44cfd7109405c2527fbf3bb687911b8922b056
Pavitrakannan <117350160+Pavitrakannan@users.noreply.github.com>
2024-02-02 13:47:07 +0530

    Update demoprog.py

--- demoprog.py
+++ demoprog.py
@@ -1,25 +1,31 @@
 print("This is my Demo Program")
-import turtle as t
-pen = t.Turtle()
-t.bgcolor('#9966cc')
-t.delay(8)
-pen.color('#ffe4e1')
-pen.begin_fill()
-pen.left(40)
-pen.forward(120)
-pen.circle(80, 190)
-pen.right(100)
-pen.circle(80, 180)
-pen.forward(160)
-pen.left(90)
-pen.forward(50)
-pen.setpos(-60, 100)
-pen.end_fill()
-def txt():
-    pen.up()
-    pen.setpos(-60, 100)
-    pen.color('red')
-    pen.write('I LoVe PyThON', font=("Comic Sans MS", 16))
-txt()
-pen.end_fill()
-t.exitonclick()
+# define size n = even only
+n = 8
+ 
+# so this heart can be made n//2 part left,
+# n//2 part right, and one middle line
+# i.e; columns m = n + 1
+m = n+1
+ 
+# loops for upper part
+for i in range(n//2-1):
+    for j in range(m):
+         
+        # condition for printing stars to GFG upper line
+        if i == n//2-2 and (j == 0 or j == m-1):
+            print("*", end=" ")
+             
+        # condition for printing stars to left upper
+        elif j <= m//2 and ((i+j == n//2-3 and j <= m//4) \
+                            or (j-i == m//2-n//2+3 and j > m//4)):
+            print("*", end=" ")
+             
+        # condition for printing stars to right upper
+        elif j > m//2 and ((i+j == n//2-3+m//2 and j < 3*m//4) \
+                           or (j-i == m//2-n//2+3+m//2 and j >= 3*m//4)):
+            print("*", end=" ")
+             
+        # condition for printing spaces
+        else:
+            print(" ", end=" ")
+    print()
